30cf19a44aecd4766c5c799dea8d392909358584
Juan Esteban Caicedo <juanes401890@gmail.com>
2026-01-01 18:26:33 +0100

    Cambios para el apartado opcional

--- 22_reliable/reliable.c
+++ 22_reliable/reliable.c
@@ -14,133 +14,125 @@
 
 #include "rlib.h"
 
-// --- CONSTANTES ---
-// Usamos MAX_PAYLOAD si existe, si no, definimos un valor seguro
 #ifndef MAX_PAYLOAD
 #define MAX_PAYLOAD 500
 #endif
 
-// --- VARIABLES GLOBALES ---
-static char last_payload[MAX_PAYLOAD]; // Buffer para guardar los datos
-static int last_payload_size;          // Tamaño de los datos guardados
+#define MAX_WINDOW_BUFFER 256
 
-static int waiting_for_ack;
+typedef struct {
+    char data[MAX_PAYLOAD];
+    int size;
+} packet_buffer_item;
+
+static packet_buffer_item tx_buffer[MAX_WINDOW_BUFFER];
+
+static uint32_t send_base;
 static uint32_t next_seqno;
+static int window_size_limit;
+
 static uint32_t expected_seqno;
+
 static long timeout_val;
 
-/*
- * INICIALIZACIÓN
- */
 void connection_initialization(int window_size, long timeout_in_ns)
 {
-    waiting_for_ack = 0;
-    next_seqno = 1;     // Empezamos secuencia en 1
-    expected_seqno = 1; // Esperamos recibir el 1
+    if (window_size > MAX_WINDOW_BUFFER) {
+        window_size = MAX_WINDOW_BUFFER;
+    }
+    window_size_limit = window_size;
     timeout_val = timeout_in_ns;
-    last_payload_size = 0;
 
-    memset(last_payload, 0, MAX_PAYLOAD);
+    send_base = 1;
+    next_seqno = 1;
+    expected_seqno = 1;
+
+    memset(tx_buffer, 0, sizeof(tx_buffer));
 }
 
-/*
- * RECEPCIÓN
- */
 void receive_callback(packet_t *pkt, size_t pkt_size)
 {
-
-    // 1. VALIDAR INTEGRIDAD
-    // Si la macro dice que está mal, lo ignoramos.
-    if (VALIDATE_CHECKSUM(pkt) == 0)
-    {
+    if (VALIDATE_CHECKSUM(pkt) == 0) {
         return;
     }
 
-    // 2. IDENTIFICAR TIPO DE PAQUETE
-    // Usamos la macro IS_ACK_PACKET para no liarnos con tamaños
-    if (IS_ACK_PACKET(pkt))
-    {
-
-        // Convertimos el ackno a formato host para comparar
+    if (IS_ACK_PACKET(pkt)) {
+        
         uint32_t ack_val = ntohl(pkt->ackno);
 
-        // Si es el ACK que esperamos (confirma el que acabamos de enviar)
-        if (waiting_for_ack && ack_val >= (next_seqno - 1))
-        {
-            SET_TIMER(0, -1); // Apagar timer
-            waiting_for_ack = 0;
+        if (ack_val > send_base) {
+            
+            send_base = ack_val;
+
+            if (send_base == next_seqno) {
+                SET_TIMER(0, -1);
+            } else {
+                SET_TIMER(0, timeout_val);
+            }
+
             RESUME_TRANSMISSION();
         }
     }
-    else
-    {
-        // ES UN PAQUETE DE DATOS
+    else {
         uint32_t seq_val = ntohl(pkt->seqno);
 
-        // ¿Es el número de secuencia exacto que esperamos?
-        if (seq_val == expected_seqno)
-        {
-
-            // Calculamos longitud de datos
+        if (seq_val == expected_seqno) {
+            
             int data_len = ntohs(pkt->len) - DATA_PACKET_HEADER;
-
-            if (data_len > 0)
-            {
-                if (data_len > MAX_PAYLOAD)
-                    data_len = MAX_PAYLOAD;
+            
+            if (data_len > 0) {
+                if (data_len > MAX_PAYLOAD) data_len = MAX_PAYLOAD;
                 ACCEPT_DATA(pkt->data, data_len);
             }
             expected_seqno++;
         }
-
-        // SIEMPRE ENVIAR ACK (CONFIRMACIÓN ACUMULATIVA)
-        // Usamos la macro que calcula checksums y cabeceras sola
+        
         SEND_ACK_PACKET(expected_seqno);
     }
 }
 
-/*
- * ENVÍO
- */
 void send_callback()
 {
-
-    if (waiting_for_ack)
-    {
+    if (next_seqno >= send_base + window_size_limit) {
         PAUSE_TRANSMISSION();
         return;
     }
 
-    // Leemos datos de la aplicación
-    int bytes_read = READ_DATA_FROM_APP_LAYER(last_payload, MAX_PAYLOAD);
+    int buffer_idx = next_seqno % MAX_WINDOW_BUFFER;
 
-    if (bytes_read > 0)
-    {
-        last_payload_size = bytes_read;
+    int bytes_read = READ_DATA_FROM_APP_LAYER(tx_buffer[buffer_idx].data, MAX_PAYLOAD);
 
-        // --- ENVIAR USANDO LA MACRO ---
-        // SEND_DATA_PACKET(longitud_total, ackno, seqno, puntero_datos)
-        // La macro se encarga de htons, htonl, checksum y memset.
-        SEND_DATA_PACKET(bytes_read + DATA_PACKET_HEADER, 0, next_seqno, last_payload);
+    if (bytes_read > 0) {
+        tx_buffer[buffer_idx].size = bytes_read;
 
-        SET_TIMER(0, timeout_val);
+        SEND_DATA_PACKET(bytes_read + DATA_PACKET_HEADER, 0, next_seqno, tx_buffer[buffer_idx].data);
+
+        if (send_base == next_seqno) {
+            SET_TIMER(0, timeout_val);
+        }
+
         next_seqno++;
-        waiting_for_ack = 1;
-        PAUSE_TRANSMISSION();
+
+        if (next_seqno >= send_base + window_size_limit) {
+            PAUSE_TRANSMISSION();
+        }
     }
 }
 
-/*
- * RETRANSMISIÓN
- */
 void timer_callback(int timer_number)
 {
-    if (timer_number == 0 && waiting_for_ack)
-    {
+    if (timer_number == 0) {
+        
+        uint32_t i;
+        for (i = send_base; i < next_seqno; i++) {
+            
+            int buffer_idx = i % MAX_WINDOW_BUFFER;
+            
+            char *data_ptr = tx_buffer[buffer_idx].data;
+            int size = tx_buffer[buffer_idx].size;
 
-        // REENVIAR EL ÚLTIMO PAQUETE
-        // Nota: Enviamos (next_seqno - 1) porque next_seqno ya avanzó
-        SEND_DATA_PACKET(last_payload_size + DATA_PACKET_HEADER, 0, next_seqno - 1, last_payload);
+            SEND_DATA_PACKET(size + DATA_PACKET_HEADER, 0, i, data_ptr);
+        }
 
         SET_TIMER(0, timeout_val);
     }
